48e5b6bc78710b1073c1364bdf57b950bfb1eb64
MangJuan08 <72972235+MangJuan08@users.noreply.github.com>
2023-03-10 19:49:20 +0100

    Added New Component

--- react-corso/src/App.js
+++ react-corso/src/App.js
@@ -1,7 +1,10 @@
+import { Component1 } from "./Component1";
+
 function App() {
   return (
     <div className="App">
       <p>CIAO!</p>
+      <Component1></Component1>
     </div>
   );
 }
---
+++ react-corso/src/Component1.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Component1 = () => {
+  return (
+    <div><p>Component1</p></div>
+  )
+}
