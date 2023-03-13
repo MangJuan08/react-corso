3ad293f3120b8fa1f33c11b8c2c73c2751a01974
MangJuan08 <72972235+MangJuan08@users.noreply.github.com>
2023-03-13 10:55:17 +0100

    Spread Operator

--- react-corso/src/App.js
+++ react-corso/src/App.js
@@ -1,5 +1,5 @@
 import { Card } from "./Card";
-import { Component1 } from "./Component1";
+
 
 const primaCard = {
   nome: "Prima Card",
@@ -12,25 +12,23 @@ const secondoCard = {
 };
 
 function App() {
+  //spread operator -> passare i valori in modo semplice
+  // const numeri = [1,2,3,4,5];
+  // console.log(Math.max(...numeri))
   return (
     <div className="container">
       <br></br>
-      <br />
-      <p>CIAO!</p>
-      <Component1></Component1>
-      <br></br>
+
       <div className="row">
         <div className="col-sm-2">
-          <Card nome={primaCard.nome} prezzo={primaCard.prezzo} />
+          <Card {...primaCard} />
         </div>
         <div className="col-sm-2">
-          <Card nome={secondoCard.nome} prezzo={secondoCard.prezzo} />
+          <Card {...secondoCard} />
         </div>
       </div>
     </div>
   );
 }
-
-
 
 export default App;
