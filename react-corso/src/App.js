524378802d4661ea9f0602e2faaa0cf8e46e7799
MangJuan08 <72972235+MangJuan08@users.noreply.github.com>
2023-03-13 10:22:22 +0100

    Nuove Modifiche Props

--- react-corso/src/App.js
+++ react-corso/src/App.js
@@ -1,34 +1,47 @@
-import { CardComponent } from "./CardComponent";
 import { Component1 } from "./Component1";
+
+const primaCard = {
+  nome: "Prima Card",
+  prezzo: 25,
+};
+
+const secondoCard = {
+  nome: "Secondo Card",
+  prezzo: 15,
+};
 
 function App() {
   return (
     <div className="container">
+      <br></br>
+      <br />
       <p>CIAO!</p>
       <Component1></Component1>
       <br></br>
       <div className="row">
         <div className="col-sm-2">
-          <CardComponent />
+          <CardComponent nome={primaCard.nome} prezzo={primaCard.prezzo} />
         </div>
         <div className="col-sm-2">
-          <CardComponent />
-        </div>
-        <div className="col-sm-2">
-          <CardComponent />
-        </div>
-        <div className="col-sm-2">
-          <CardComponent />
-        </div>
-        <div className="col-sm-2">
-          <CardComponent />
-        </div>
-        <div className="col-sm-2">
-          <CardComponent />
+          <CardComponent nome={secondoCard.nome} prezzo={secondoCard.prezzo} />
         </div>
       </div>
     </div>
   );
 }
+
+const CardComponent = (props) => {
+ 
+  return (
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">{props.nome}</h5>
+        <p>
+        €{props.prezzo}
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default App;
