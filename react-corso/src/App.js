322f1d719e9afa0a3993dca715bffac48c6ec76b
MangJuan08 <72972235+MangJuan08@users.noreply.github.com>
2023-03-13 12:18:37 +0100

    KEY

--- react-corso/src/App.js
+++ react-corso/src/App.js
@@ -1,34 +1,93 @@
 import { Card } from "./Card";
 
-
-const primaCard = {
-  nome: "Prima Card",
-  prezzo: 25,
-};
-
-const secondoCard = {
-  nome: "Secondo Card",
-  prezzo: 19,
-};
+const products = [
+  {
+    id: 0,
+    nome: "Colnago V3Rs",
+    prezzo: "6.0000",
+    img: process.env.PUBLIC_URL + "/images/colnagov3rs.jpg",
+  },
+  {
+    id: 1,
+    nome: "Colnago V3",
+    prezzo: "3.000",
+    img: process.env.PUBLIC_URL + "/images/colnagov3.jpg",
+  },
+  {
+    id: 2,
+    nome: "Colnago V4",
+    prezzo: "3.000",
+    img: process.env.PUBLIC_URL + "/images/colnagov3.jpg",
+  },
+  {
+    id: 3,
+    nome: "Colnago V5",
+    prezzo: "3.000",
+    img: process.env.PUBLIC_URL + "/images/colnagov3.jpg",
+  },
+  {
+    id: 4,
+    nome: "Colnago V6",
+    prezzo: "3.000",
+    img: process.env.PUBLIC_URL + "/images/colnagov3.jpg",
+  },
+  {
+    id: 5,
+    nome: "Colnago V7",
+    prezzo: "3.000",
+    img: process.env.PUBLIC_URL + "/images/colnagov3.jpg",
+  },
+  {
+    id: 6,
+    nome: "Colnago V8",
+    prezzo: "3.000",
+    img: process.env.PUBLIC_URL + "/images/colnagov3.jpg",
+  },
+  {
+    id: 7,
+    nome: "Colnago V9",
+    prezzo: "3.000",
+    img: process.env.PUBLIC_URL + "/images/colnagov3.jpg",
+  },
+];
 
 function App() {
   //spread operator -> passare i valori in modo semplice
   // const numeri = [1,2,3,4,5];
   // console.log(Math.max(...numeri))
+  //props.children-> tutto il contenuto dall'apertura e chiusa del tag del componente
+  //key => serve per far rendere univoco l'oggetto
   return (
     <div className="container">
       <br></br>
 
       <div className="row">
-        <div className="col-sm-2">
-          <Card {...primaCard} />
-        </div>
-        <div className="col-sm-2">
-          <Card {...secondoCard} />
-        </div>
+        {products.map((products) => {
+          return (
+            <Card {...products} key={products.id}>
+              <>BELLA BICI!</>
+            </Card>
+          );
+        })}
       </div>
     </div>
   );
 }
 
 export default App;
+/*
+
+<Card {...primaCard}>
+            <button type="button" className="btn btn-primary">
+              Dettagli
+            </button>
+          </Card>
+        </div>
+        <div className="col-sm-3">
+          <Card {...secondoCard}>
+            <button type="button" className="btn btn-primary">
+              Dettagli
+            </button>
+          </Card>
+
+*/
--- react-corso/src/Card.js
+++ react-corso/src/Card.js
@@ -1,14 +1,26 @@
-import React from 'react'
+import React from "react";
 
-export const Card = (props) => {
+export const Card = ({ nome, prezzo, img, children, key }) => {
+  console.log(key)
+  //decostruzione è l'oppost dello spread operator
+  //dichiarare le chiavi che corrispondo alle valori
+
+  ///decostruzione const {nome,prezzo, img, children} = props;
+
   return (
-    <div className="card">
-    <div className="card-body">
-      <h5 className="card-title">{props.nome}</h5>
-      <p>
-      €{props.prezzo}
-      </p>
+    <div className="col-md-3">
+      <div className="card" style={{ marginTop: 20 }}>
+        <div className="card-body">
+          <h5 className="card-title text-uppercase">{nome}</h5>
+          <img src={img} alt="Bike" width="150" height="100"></img>
+          <br></br> <br></br>
+          <p>€{prezzo}</p>
+          <p>{children}</p>
+          <button type="button" className="btn btn-primary">
+            Dettagli
+          </button>
+        </div>
+      </div>
     </div>
-  </div>
-  )
-}
+  );
+};
