9fbb97fefaa95d779220e65fea6f09e75dff9cdf
MangJuan08 <72972235+MangJuan08@users.noreply.github.com>
2023-03-13 12:22:02 +0100

    creazione de nuovo file Products.js per i prodotti

--- react-corso/src/App.js
+++ react-corso/src/App.js
@@ -1,55 +1,7 @@
 import { Card } from "./Card";
+import products from "./Products";
 
-const products = [
-  {
-    id: 0,
-    nome: "Colnago V3Rs",
-    prezzo: "6.0000",
-    img: process.env.PUBLIC_URL + "/images/colnagov3rs.jpg",
-  },
-  {
-    id: 1,
-    nome: "Colnago V3",
-    prezzo: "3.000",
-    img: process.env.PUBLIC_URL + "/images/colnagov3.jpg",
-  },
-  {
-    id: 2,
-    nome: "Colnago V4",
-    prezzo: "3.000",
-    img: process.env.PUBLIC_URL + "/images/colnagov3.jpg",
-  },
-  {
-    id: 3,
-    nome: "Colnago V5",
-    prezzo: "3.000",
-    img: process.env.PUBLIC_URL + "/images/colnagov3.jpg",
-  },
-  {
-    id: 4,
-    nome: "Colnago V6",
-    prezzo: "3.000",
-    img: process.env.PUBLIC_URL + "/images/colnagov3.jpg",
-  },
-  {
-    id: 5,
-    nome: "Colnago V7",
-    prezzo: "3.000",
-    img: process.env.PUBLIC_URL + "/images/colnagov3.jpg",
-  },
-  {
-    id: 6,
-    nome: "Colnago V8",
-    prezzo: "3.000",
-    img: process.env.PUBLIC_URL + "/images/colnagov3.jpg",
-  },
-  {
-    id: 7,
-    nome: "Colnago V9",
-    prezzo: "3.000",
-    img: process.env.PUBLIC_URL + "/images/colnagov3.jpg",
-  },
-];
+
 
 function App() {
   //spread operator -> passare i valori in modo semplice
