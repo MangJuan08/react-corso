4a0623e3eec10c2980316b230b398371586dac1c
MangJuan08 <72972235+MangJuan08@users.noreply.github.com>
2023-03-14 14:10:48 +0100

    export QUASI FATTO

--- file-converter/src/App.js
+++ file-converter/src/App.js
@@ -1,24 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FileUploader } from "react-drag-drop-files";
-
+import * as XLSX from "xlsx/xlsx";
 import "./styles.css";
 
 const fileTypes = ["PDF"];
 
 export default function App() {
   const [file, setFile] = useState(null);
-  const [names,setName] = useState(null)
-  let arr = []
+  const [sheetData, setSheetData]= useState(null);
+  const arr = [
+    ["prova","prova"]
+  ]
+
+  useEffect(() => {
+    setSheetData(arr);
+  }, [])
+  
+
   const handleChange = (file) => {
     setFile(file);
     console.log(typeof file);
-   
-    /*Object.keys(file).map((key) => (
- arr.push(file[key].name.replace(/_/g, " ")
-    )) 
-
-    console.log(arr)*/
   };
+
+  const handleExport = () => {
+    let wb = XLSX.utils.book_new(),
+    ws = XLSX.utils.json_to_sheet(sheetData);
+  
+    XLSX.utils.book_append_sheet(wb, ws, "Sheet1");
+    XLSX.writeFile(wb, "MyExcel.xlsx");
+  }
+
   return (
     <div className="App">
       <h1>Hello To Drag & Drop Files</h1>
@@ -28,12 +39,15 @@ export default function App() {
         name="file"
         types={fileTypes}
       />
+      <p>{ file ? file.length : "NO FILES UPLOADED"}</p>
       <ul>
         {file ? Object.keys(file).map((key) => (
           <li key={key}>{file[key].name.replace(/_/g, " ")}</li>
         )) 
-      : "UPLOAD FILES"}
+      : ""}
       </ul>
+
+      <button className="btn btn-primary" onClick={handleExport}>EXPORT</button>
     </div>
   );
 }
